c0bd556b55d4d3536fb8fcece972fa906d3b94cf
DohyunMAC <dohyun11111m@gmail.com>
2025-11-15 22:57:01 +0900

    changed font weight of buttons

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -163,7 +163,7 @@ export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     const variantStyles = getVariantStyles();
     const hoverBgColor = getHoverBackgroundColor();
 
-    const baseClasses = 'inline-flex items-center justify-center font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:cursor-not-allowed';
+    const baseClasses = 'inline-flex items-center justify-center font-normal transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:cursor-not-allowed';
     
     const sizeClass = sizeClasses[size] || sizeClasses.lg;
     const variantClass = variantClasses[actualVariant]?.[actualColor] || variantClasses.solid.primary;
